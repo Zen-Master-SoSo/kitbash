19dfdbc2014996a852703f06e51ecbd33e9d2da3
Liyang <liyang@holly.home>
2025-12-28 17:49:04 +0800

    Replace type(<object>).__name__ with <object>.__class__.__name__

--- kitbash/gui/__init__.py
+++ kitbash/gui/__init__.py
@@ -63,7 +63,7 @@ class GeometrySaver:
 	def restore_geometry(self):
 		if not hasattr(self, 'restoreGeometry'):
 			logging.error('Object of type %s has no "restoreGeometry" function',
-				type(self).__name__)
+				self.__class__.__name__)
 			return
 		geometry = settings().value(self.__geometry_key())
 		if geometry is not None:
@@ -76,17 +76,17 @@ class GeometrySaver:
 	def save_geometry(self):
 		if not hasattr(self, 'saveGeometry'):
 			logging.error('Object of type %s has no "saveGeometry" function',
-				type(self).__name__)
+				self.__class__.__name__)
 			return
 		settings().setValue(self.__geometry_key(), self.saveGeometry())
 		for splitter in self.findChildren(QSplitter):
 			settings().setValue(self.__splitter_geometry_key(splitter), splitter.saveState())
 
 	def __geometry_key(self):
-		return '{}/geometry'.format(type(self).__name__)
+		return '{}/geometry'.format(self.__class__.__name__)
 
 	def __splitter_geometry_key(self, splitter):
-		return '{}/{}/geometry'.format(type(self).__name__, splitter.objectName())
+		return '{}/{}/geometry'.format(self.__class__.__name__, splitter.objectName())
 
 
 #  end kitbash/gui/__init__.py
